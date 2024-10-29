d89d1ed2f32d9b7d5e3a3cd8da58e3e06fac0d1a
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-10-29 17:42:57 -0500

    Time: 53 ms (69.23%) | Memory: 31.4 MB (11.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,14 +3,15 @@ class Solution:
         # we can take a greedy approach, where we simply just loook for greater elements 
         # we can keep track of a prefix sum where if it is < 0, = 0
         # however, we need to start off sum with first value 
-        # if l pointer is negative, then we iterate the left pointer until thr right
 
-        l,r = 0, 0
-        maxSum = max(nums)
+        r = 0
+        maxSum = nums[0]
         prefixSum = 0
         
         while r  < len(nums):
             prefixSum += nums[r]
+            if nums[r] > maxSum:
+                maxSum = nums[r]
             if prefixSum < 0:
                 prefixSum = 0
             else:
