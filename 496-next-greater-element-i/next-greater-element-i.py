d94cc1de40f1da81366cc26d3fedb3ff53ed9712
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-11-01 18:12:42 -0500

    Time: 18 ms (24.34%) | Memory: 16.9 MB (10.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-
+        '''
         greaterPostFix = [-1] * len(nums2)
         # -1 is gonna indicate that there is no greater number
         # any number will indicate that is the greatest number 
@@ -15,8 +15,29 @@ class Solution:
         for index, number in enumerate(nums1):
             if number in nums2:
                 res.append(greaterPostFix[nums2.index(number)])
+        
         print(greaterPostFix)
         return res
+        '''
+        # have a stack that runs through and sees if theres anythign bigger than it 
+
+        res = [0] * len(nums2)
+        stack = []
+
+        for i, value in enumerate(nums2):
+            while stack and value > nums2[stack[-1]]:
+                index = stack.pop()
+                res[index] = value
+            stack.append(i)
+        ans = []
+        while stack:
+            index = stack.pop()
+            res[index] = -1
+        print(res)
+        for num in nums1:
+            ans.append(res[nums2.index(num)])
+        return ans
+
 
 
     
