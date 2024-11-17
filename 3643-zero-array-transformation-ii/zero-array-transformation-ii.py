69af1db81cc2e9f794ad9c1fcbb9513d932a1522
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-11-16 22:43:29 -0600

    Time: 1233 ms (100.00%) | Memory: 57.2 MB (100.00%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -0,0 +1,49 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+
+        l, r = 0, len(queries)
+        found = False
+        
+        while l < r:
+            middle = (l + r) // 2
+            result = self.findZeroArray(nums, queries, middle)
+            if result:
+                r = middle
+                found = True
+                lastFoundValue = middle
+            else:
+                l = middle + 1
+            
+        if found:
+            return l
+        else:
+            result = self.findZeroArray(nums, queries, len(queries))
+            if result:
+                return len(queries)
+            return -1
+
+
+
+    def findZeroArray(self, nums, queries, middle):
+        copy = []
+        copy[:] = nums
+        
+        track = [0] * (len(copy) + 1)
+        
+        for index in range(middle):
+            left = queries[index][0]
+            right = queries[index][1]
+            value = queries[index][2]
+            track[left] -= value
+            track[right + 1] += value
+            
+        currSubtract = 0 
+        
+        for index in range(len(copy)):
+            currSubtract += track[index]
+            copy[index] += currSubtract
+        
+        for num in copy:
+            if num > 0:
+                return False
+        return True
