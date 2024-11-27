f145e5775cabe018d176a46b2b2af06abca73e64
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-11-27 17:09:23 -0600

    Time: 148 ms (48.82%) | Memory: 62.6 MB (12.53%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -4,12 +4,15 @@ class Solution:
         # you would solve the current array length 
         # then you would have to check if the target number - currSum is in the prefixSum
         # calculate the index and then maximize the width of it 
-
+        # we have to hceck currSum - k, we can either make a hashamp liek this 
+        # or we can just see if it ever becomes that then we use it 
+        
         prefixSum = {0:0}
         currSum = 0
         maxSubArray = False
 
         for index, num in enumerate(nums, start = 1):
+
             currSum += nums[index - 1]
             targetNum = currSum - k
             if targetNum in prefixSum:
