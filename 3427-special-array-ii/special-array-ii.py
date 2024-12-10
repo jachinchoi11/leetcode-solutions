6aa113baf098987ed2970f6c2caf8930c442760c
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-12-09 18:26:02 -0600

    Time: 35 ms (88.11%) | Memory: 47.3 MB (25.58%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -3,14 +3,14 @@ class Solution:
         res = [False] * len(queries)
         # we have to do prefix sums in this case and compute which how many bad indexes there are 
 
-        prefixSum = {0:0}
+        prefixSum = [0] * len(nums)
 
         for index in range(1, len(nums)):
             if nums[index] % 2 == nums[index - 1] % 2:
                 prefixSum[index] = prefixSum[index - 1] + 1
             else:
                 prefixSum[index] = prefixSum[index - 1]
-        print(prefixSum)
+        
         currIndex = 0
         for start, end in queries:
             if prefixSum[end] - prefixSum[start] == 0:
