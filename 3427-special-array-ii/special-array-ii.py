ea6397b0ffb23f94d30ff43e7db92cfed30e1b88
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-12-09 18:24:32 -0600

    Time: 83 ms (24.67%) | Memory: 56 MB (7.36%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        res = [False] * len(queries)
+        # we have to do prefix sums in this case and compute which how many bad indexes there are 
+
+        prefixSum = {0:0}
+
+        for index in range(1, len(nums)):
+            if nums[index] % 2 == nums[index - 1] % 2:
+                prefixSum[index] = prefixSum[index - 1] + 1
+            else:
+                prefixSum[index] = prefixSum[index - 1]
+        print(prefixSum)
+        currIndex = 0
+        for start, end in queries:
+            if prefixSum[end] - prefixSum[start] == 0:
+                res[currIndex] = True
+            else:
+                res[currIndex] = False
+            currIndex += 1
+        return res
+            
