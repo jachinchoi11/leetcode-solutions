b2923419c126d9d36541b4155340f85818ac9704
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-12-10 17:04:15 -0600

    Time: 252 ms (44.49%) | Memory: 19.6 MB (49.78%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,7 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        count = 0
+        # dfs solution
+        '''count = 0
 
         def dfs(row, col):   
             if min(row, col) < 0 or row == len(grid) or col == len(grid[0]) or grid[row][col] != '1':
@@ -23,4 +23,30 @@ class Solution:
         return count
         
         
+        '''
+        # bfs solution 
+
+        num_of_islands = 0
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '0':
+                    continue
+                queue.append((row, col))
+                grid[row][col] == '0'
+                while queue:
+                    currRow, currCol = queue.popleft()
+                    adj = [[1,0], [0,1], [-1,0], [0,-1]]
+                    for dx, dy in adj:
+                        newRow = currRow + dx
+                        newCol = currCol + dy
+                        if min(newRow, newCol) < 0 or newRow == rows or newCol == cols or grid[newRow][newCol] == '0':
+                            continue
+                        queue.append((newRow, newCol))
+                        grid[newRow][newCol] = '0'
+                num_of_islands += 1
+        return num_of_islands
+
         
