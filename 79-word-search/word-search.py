8e55bf608732fa1075cb1c6c82be24ee81467230
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2024-12-11 18:28:40 -0600

    Time: 6524 ms (10.87%) | Memory: 17.6 MB (5.25%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # go through the array until you see first letter and then run a dfs and see if you're able to get through to very end
+        # keep track of this using a global variable and then you can note it down somewhere 
+
+        rows, cols = len(board), len(board[0])
+        visited = set()
+        self.found = False
+
+        def dfs(row, col, index, currVisited):
+            if index == len(word):
+                self.found = True
+                return
+            if row < 0 or col < 0 or row == rows or col == cols or (row, col) in currVisited or board[row][col] != word[index]:
+                return
+            
+            currVisited.append((row, col))
+            dfs(row + 1, col, index + 1, currVisited)
+            dfs(row - 1, col, index + 1, currVisited)
+            dfs(row, col + 1, index + 1, currVisited)
+            dfs(row, col - 1, index + 1, currVisited)
+            currVisited.pop()
+        
+        currVisited = []
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] != word[0] or (row, col) in visited:
+                    continue
+                else:
+                    dfs(row, col, 0, currVisited)
+                    visited.add((row, col))
+        return self.found
