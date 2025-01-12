56812d953eef52aacc7baa86f0e7138380398003
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-12 16:37:36 -0600

    Time: 281 ms (13.10%) | Memory: 37.6 MB (6.11%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         # i think we can use a heap to keep track of the top two minimum numbers 
-        # and the number that we choose to do 
+        # and the number that we choose to do is to find how many there are 
 
-        num_less_than_k = 0 
+        '''num_less_than_k = 0 
         minHeap = []
         operation_count = 0
 
@@ -27,12 +27,19 @@ class Solution:
         if minHeap and num_less_than_k > 0:
             operation_count += 1
         
+        return operation_count'''
+
+        minHeap = []
+        operation_count = 0
+        for num in nums:
+            heapq.heappush(minHeap, num)
+        
+        while len(minHeap) >= 2 and minHeap[0] < k:
+            operation_count += 1
+            num1 = heapq.heappop(minHeap)
+            num2 = heapq.heappop(minHeap)
+            newNum = (2 * min(num1, num2) + max(num1, num2))
+            heapq.heappush(minHeap, newNum)
+        
         return operation_count
 
-        # 2,11,10,1,3
-        # 1,2,3,10,11
-        # num less = 2
-        # operations = 2
-        # 2,3,5,10, 11
-        # 3,5,10,11,11
-        #
