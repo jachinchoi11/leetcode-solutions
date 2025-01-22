df91fa296560fe6b8b80358f7be1b3d86b42536c
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-22 14:52:07 -0600

    Time: 70 ms (63.75%) | Memory: 33.6 MB (19.09%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        
+        # i think we can have a hashmap that will show what the pairs are 
+            # the way we can take advantage of the hashamp approach is using the k to mod it 
+            # and see what we woudl need 
+
+        # res = 0 --> should equal the len(arr) // 2
+
+        divisors = defaultdict(int)
+
+        for num in arr:
+            converted_num = num % k
+            divisors[converted_num] += 1
+        
+        for key, value in divisors.items():
+            target_divisor = k - key
+            if key == 0:
+                continue
+            elif key == target_divisor:
+                if divisors[key] % 2 != 0:
+                    return False
+            else:
+                if target_divisor in divisors:
+                    if value != divisors[target_divisor]:
+                        return False
+                else:
+                    return False
+        return True
