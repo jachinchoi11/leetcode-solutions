239fef1fa6dca6df05196c5822e19ae9f0584448
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-23 10:47:05 -0600

    Time: 12 ms (51.03%) | Memory: 18.3 MB (10.57%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,33 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        '''
+        can we just assign it in a hashmap with the values
+        '''
+
+        res = [[] for _ in range(numRows)]
+        convert = []
+        pointer = 0
+        increasing = True
+        index = 0
+
+        if numRows == 1 or numRows >= len(s):
+            return s
+        for char in s:
+            res[pointer].append(char)
+            if increasing:
+                pointer += 1
+            else:
+                pointer -= 1
+            if pointer == numRows - 1:
+                increasing = False
+            if pointer == 0:
+                increasing = True
+        for each_list in res:
+            convert.extend(each_list)
+        
+        return "".join(convert)
+            
+
+
+
