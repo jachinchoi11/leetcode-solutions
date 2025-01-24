1cc56d26b5cb5a31d401f891dc29aaf6d9ec8586
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-24 13:09:24 -0600

    Time: 28 ms (86.79%) | Memory: 24.5 MB (11.53%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,28 +1,26 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-
+        # the more optimal appraochw woudl be to keep track of whether it is safe or not 
         res = []
         number_of_nodes = len(graph)
-        safe = set()
+        safe = {}
         visited = set()
         
         def dfs(index, visited):
-            if index in safe or graph[index] == []:
-                return True
+            if index in safe:
+                return safe[index]
             if index in visited:
                 return False
+            safe[index] = False
             visited.add(index)
             for neighbors in graph[index]:
                 if not dfs(neighbors, visited):
                     return False
             visited.remove(index)
-            safe.add(index)
+            safe[index] = True
             return True
-
+    
         for index in range(number_of_nodes):
-            if graph[index] == [] or index in safe:
+            if dfs(index, visited):
                 res.append(index)
-            else:
-                if dfs(index, visited):
-                    res.append(index)
         return res
