e834a31cd41f3b6e84801784567ba4826a40d860
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-24 13:03:27 -0600

    Time: 54 ms (46.94%) | Memory: 24 MB (24.29%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,19 +1,11 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        
-        # i think we can do a bfs on this but the directions are that we have to go and reach a terminal node for all the different paths 
-        # before we can move on 
-        # if we see a terminal node, bascialy the graph[index] = [], then we can say taht path is fine 
-        # and then we can essentially add that node into the safe node, as a way to not redo that computation 
-        # then that way if anythign points to it can be seen as a safe index, (so we will use a set)
-        # the first iteration, we can figure it out for the actual terminal nodes, then we can output it in order 
-        # the reason why a bfs might not be as good is to actually seek that cycle 
-
 
         res = []
         number_of_nodes = len(graph)
         safe = set()
         visited = set()
+        
         def dfs(index, visited):
             if index in safe or graph[index] == []:
                 return True
@@ -33,5 +25,4 @@ class Solution:
             else:
                 if dfs(index, visited):
                     res.append(index)
-                    safe.add(index)
         return res
