bf650b7daf469a22dde8166fe6ab681dcb627bd8
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-24 09:51:21 -0600

    Time: 42 ms (61.37%) | Memory: 24.1 MB (17.52%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -13,7 +13,7 @@ class Solution:
         res = []
         number_of_nodes = len(graph)
         safe = set()
-
+        visited = set()
         def dfs(index, visited):
             if index in safe or graph[index] == []:
                 return True
@@ -27,12 +27,11 @@ class Solution:
             safe.add(index)
             return True
 
-
         for index in range(number_of_nodes):
             if graph[index] == [] or index in safe:
                 res.append(index)
             else:
-                if dfs(index, set()):
+                if dfs(index, visited):
                     res.append(index)
                     safe.add(index)
         return res
