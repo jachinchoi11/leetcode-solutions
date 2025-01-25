0817f6bcded86af24a8075371499915ee01a107e
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-25 01:34:28 -0600

    Time: 252 ms (5.02%) | Memory: 44.3 MB (11.04%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        
+        '''        
         # for a diagonal order, you have to realize that if you're on the same sum of rows and columns 
 
         # so maybe we can group by sum of (rows and columns)
@@ -24,4 +24,26 @@ class Solution:
             for item in reversed(count[currNum]):
                 res.append(item)
             currNum += 1
+        return res'''
+
+        # a really cool bfs approach that would be one pass, essentially you would bfs into the right and the bottom directions and append as you go 
+        # make sure to prioritize the downward first and then also have a visited set 
+
+        queue = deque()
+        queue.append((0, 0))
+        visited = set()
+        res = []
+
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                for dr, dc in [[1, 0], [0, 1]]:
+                    newR = dr + row
+                    newC = dc + col
+                    if newR >= len(nums) or newC >= len(nums[newR]):
+                        continue
+                    if (newR, newC) not in visited:
+                        queue.append((newR, newC))
+                        visited.add((newR, newC))
+                res.append(nums[row][col])
         return res
