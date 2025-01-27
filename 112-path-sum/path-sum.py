16d335fb3eb0665c5ec880b128f925bde2e84897
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-01-27 13:43:14 -0600

    Time: 0 ms (100.00%) | Memory: 19.1 MB (16.85%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,22 +6,36 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        sum = 0
-        self.hasPathSum = False
-        self.targetSum = targetSum
-        self.dfs(root, sum)
-        return self.hasPathSum
 
 
-    def dfs(self, root, sum):
+        # so i think we can do a dfs, where in each sub problem we can add, as we go and see if we ever reach the value 
+
+        # this can be a preorder traversal, as we want to evaluate the values, before we go down 
+        # we can have two base cases, one being if the sum is already bigger than the targetSum, we will terminate it and return False
+        # if the value is a leaf node and the value == targetSum, return True there 
+
+        # if the value is the value, but not a leaf node, we can also terminate ealeir where 
+
+        # if not, we will add to the currSum and and then pass it in as a parameter, so we would do curr_node, currSum 
+
+        # since it recurses back, it should redo those values, and we should not have to backtrack
         if not root:
-            return
-        sum += root.val 
+            return False
+        return self.preDfs(root, root.val, targetSum)
+        
+    def preDfs(self, curr_node, curr_sum, targetSum):
+        if not curr_node.left and not curr_node.right:
+            if curr_sum == targetSum:
+                return True
+            else:
+                return False
 
-        if sum == self.targetSum and not root.left and not root.right:
-            self.hasPathSum = True
+        left, right = False, False
+        if curr_node.left:
+            left = self.preDfs(curr_node.left, curr_sum + curr_node.left.val, targetSum)
+        if curr_node.right:
+            right = self.preDfs(curr_node.right, curr_sum + curr_node.right.val, targetSum)
 
-        self.dfs(root.left, sum)
-        self.dfs(root.right, sum)
+        return left or right
 
 
