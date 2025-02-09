fa45a02f3d99379250f7ef2319d6969d4a79f2c1
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-09 01:18:35 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.00%) - LeetSync

--- 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
@@ -8,21 +8,24 @@ class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
         # intuition - use the height of the certain tree to map the leaves 
         # but the thing is do we preallocate it, or do we just append and reverse it at the end
-        res = []
+
+        res = defaultdict(list)
+
         if not root:
             return res
         
         self.dfs(root, res)
-
-        return res
+        return list(res.values())
     
     def dfs(self, curr_node, res):
         if not curr_node:
-            return -1
-        level = 1 + max(self.dfs(curr_node.left, res), self.dfs(curr_node.right, res))
-        if len(res) <= level:
-            res.append([])
-        res[level].append(curr_node.val)
+            return 0
+        left_length = self.dfs(curr_node.left, res)
+        right_length = self.dfs(curr_node.right, res)
+        level = 1 + max(left_length, right_length)
+
+        res[level - 1].append(curr_node.val)
+
         return level
 
             
