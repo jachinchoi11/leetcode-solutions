2cc8807a479bce30e7fbdddf42474737088dc81e
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-09 20:47:28 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (70.70%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -3,22 +3,13 @@ class Solution:
         
         # so what if we had a stack that only took care of the non digits characters
         # when we see a character, pop off that index and add it to a hashset, then as we go through we don't use 
-        index_set = set()
-
-        stack = []
-
-        for index, char in enumerate(s):
-            if not char.isdigit():
-                stack.append(index)
-            else:
-                if stack:
-                    curr_index = stack.pop()
-                    index_set.add(curr_index)
-        
         res = []
 
-        for index, char in enumerate(s):
-            if char.isdigit() or index in index_set:
-                continue
-            res.append(char)
+        for char in s:
+            if not char.isdigit():
+                res.append(char)
+            else:
+                if res:
+                    res.pop()
+                
         return "".join(res)
