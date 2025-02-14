708751af8bbf1511759effcb58c344f5ca9dbd3b
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-13 21:55:20 -0600

    Time: 39 ms (52.82%) | Memory: 31.8 MB (37.60%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -4,12 +4,10 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.prefixMult = [1]  # Store prefix products (1-based indexing for easy division)
-        self.lastZeroIndex = -1  # Track last zero position
     
     def add(self, num: int) -> None:
         if num == 0:
             self.prefixMult = [1]  # Reset on zero
-            self.lastZeroIndex = len(self.prefixMult) - 1
         else:
             self.prefixMult.append(self.prefixMult[-1] * num)  # Maintain prefix product
 
