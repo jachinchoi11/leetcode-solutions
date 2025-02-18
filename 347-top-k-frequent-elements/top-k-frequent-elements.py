c049ec3f08d6582cf33784b25c41f7a0abb72b73
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-18 17:01:23 -0600

    Time: 5 ms (63.32%) | Memory: 21.1 MB (80.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,27 @@
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
+        
+        # first course of action, we count the number of times a number appears in teh array (nums)
+
+        # second one is going to be using some sort of a sorting algorithm to get the top k frequent ones
+            # in this case, since we're not doing any modficiations or adding any new values into the heap
+            # we can just sort it using the lambda sort function and output the top k elements
+        
+        counter_map = defaultdict(int)
+        unsorted_list = []
+        res = []
 
         for num in nums:
-            if num in count:
-                count[num] -= 1
-            else:
-                count[num] = -1
-            
-        heap = []
-        for key, value in count.items():
-            heapq.heappush(heap, (value, key))
-        res = []
-        for i in range(k):
-            value = heapq.heappop(heap)
-            res.append(value[1])
+            counter_map[num] += 1
         
-        return res 
+        for key, value in counter_map.items():
+            key_value_pair = [key, value]
+            unsorted_list.append(key_value_pair)
+        unsorted_list.sort(key =lambda x: -x[1])
 
+        for i in range(k):
+            res.append(unsorted_list[i][0])
 
+        return res
 
