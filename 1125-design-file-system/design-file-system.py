d07e01038f396cdb4d665765f99eaeb72de91390
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-17 19:12:07 -0600

    Time: 44 ms (55.12%) | Memory: 25.3 MB (88.61%) - LeetSync

--- 1125-design-file-system/design-file-system.py
+++ 1125-design-file-system/design-file-system.py
@@ -1,24 +1,25 @@
 class FileSystem:
-
+    # essentially, the idea of this problem is to check if soemthing is already there, so we'll have a hashmap
+    # of waht we have and assign itt o a value 
     def __init__(self):
-        self.hashmap = {}
-
+        self.path_to_value = {}
     def createPath(self, path: str, value: int) -> bool:
-        currPath = path.split('/')
-        if len(currPath) > 2:
-            parent = '/'.join(currPath[:-1])
-            print(parent)
-            if parent not in self.hashmap:
-                return False
-
-        if path in self.hashmap or path == '/':
+        if path in self.path_to_value:
             return False
         
-        self.hashmap[path] = value
+        curr_path = path.split('/')
+        if len(curr_path) > 2:
+            parent = '/'.join(curr_path[:-1])
+            if parent not in self.path_to_value:
+                return False
+        self.path_to_value[path] = value
         return True
 
     def get(self, path: str) -> int:
-        return self.hashmap[path] if path in self.hashmap else -1
+
+        if path in self.path_to_value:
+            return self.path_to_value[path]
+        return -1
 
 
 # Your FileSystem object will be instantiated and called as such:
