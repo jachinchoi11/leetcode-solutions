a6b4b66452cdfada5cd245fd4c2daf2b7319e1a7
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-02-25 00:16:45 -0600

    Time: 48 ms (56.96%) | Memory: 18.3 MB (80.67%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -6,29 +6,26 @@ class Solution:
         
         count_p = Counter(p)
         curr_count = defaultdict(int)
-        res, left = [], 0
+        res, left, right = [], 0, 0
 
         # for this one, in an anagram it has to fit all the characters in the string case
         # so now, we have to figure out the case when we do find one, actually we don't need to 
         # since this is a fixed case we can do a fixed sliding window on this rpobem 
 
-        for right in range(len(p)):
-            curr_count[s[right]] += 1
-        right += 1
-    
-
         while right < len(s):
-            if curr_count == count_p:
-                res.append(left)
             right_char = s[right]
             curr_count[right_char] += 1
-            left_char = s[left]
-            left += 1
-            curr_count[left_char] -= 1
-            if curr_count[left_char] == 0:
-                curr_count.pop(left_char)
+
+            if right >= len(p):
+                # right will be 
+                left_char = s[left]
+                left += 1
+                curr_count[left_char] -= 1
+                if curr_count[left_char] == 0:
+                    curr_count.pop(left_char)
+            
+            if curr_count == count_p:
+                res.append(left)
             right += 1
 
-        if (curr_count) == count_p:
-            res.append(left)
         return res 
