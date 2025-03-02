a1f6abc7cd4f71fecea6b0f705362280212706a9
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-02 10:18:43 -0600

    Time: 273 ms (31.62%) | Memory: 25.4 MB (22.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,52 +1,50 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # dfs solution
-        '''count = 0
-
-        def dfs(row, col):   
-            if min(row, col) < 0 or row == len(grid) or col == len(grid[0]) or grid[row][col] != '1':
-                return
-            grid[row][col] = '0'
-
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-
-
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == '0':
-                    continue
-                dfs(row, col)
-                count += 1
-        return count
         
+        # first clarifying questions I want to ask is the input always going to be in a 2d array
+        # and theo nly possibilities are land and water?
+
+        # to define what an isalnd, its land tht is adjacent to each other 
+            # what about diagonals, does diagonals count as adjacent ? no
         
-        '''
-        # bfs solution 
+        # one more thign is this input going to be resued somewhere else (in a followup)
+            # i thin kwe can save sapce and alter the grid while we go through it or use some type of visited set potentially 
+
+        # bfs: where we can kind of just go trhough the 2d grid and whenever we do see a 1 
+            # we can just look at it and see if it is in our visited if not 
+                # the we call the bfs 
+                # eveyrtime we call the bfs, we would increment our count of islands by 1
+        
+        # inside the bfs: we woudl esssetnailyl - check all teh 4 direcitons and look at if there are any 1s
+            # when we're checking all 4 firecitons, we have to mkae sure it is in bounds as well 
+            # add it to the qeue and the visited set (so it can conitnue to check all adjacent ones)
+        
 
         num_of_islands = 0
         rows, cols = len(grid), len(grid[0])
-        queue = deque()
+        visited = set()
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == '0':
-                    continue
-                queue.append((row, col))
-                grid[row][col] == '0'
-                while queue:
-                    currRow, currCol = queue.popleft()
-                    adj = [[1,0], [0,1], [-1,0], [0,-1]]
-                    for dx, dy in adj:
-                        newRow = currRow + dx
-                        newCol = currCol + dy
-                        if min(newRow, newCol) < 0 or newRow == rows or newCol == cols or grid[newRow][newCol] == '0':
-                            continue
-                        queue.append((newRow, newCol))
-                        grid[newRow][newCol] = '0'
-                num_of_islands += 1
+                if grid[row][col] == '1' and (row, col) not in visited:
+                    self.bfs(row, col, visited, rows, cols, grid)
+                    num_of_islands += 1
         return num_of_islands
 
-        
+    def bfs(self, curr_row, curr_col, visited, rows, cols, grid):
+            queue = deque([(curr_row, curr_col)])
+            directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+            visited.add((curr_row, curr_col))
+            def isValid(curr_row, curr_col):
+                if curr_row < 0 or curr_col < 0 or curr_row == rows or curr_col == cols or grid[curr_row][curr_col] == '0' or (curr_row, curr_col) in visited:
+                    return False
+                return True 
+
+            while queue:
+                curr_row, curr_col = queue.popleft()
+                for row_offset, col_offset in directions:
+                    new_row = curr_row + row_offset
+                    new_col = curr_col + col_offset
+                    if isValid(new_row, new_col):
+                        queue.append((new_row, new_col))
+                        visited.add((new_row, new_col))
