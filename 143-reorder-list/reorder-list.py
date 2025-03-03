eb01d33d98686e7841f635f890b73d98fb8937ff
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-03 14:03:11 -0600

    Time: 7 ms (21.46%) | Memory: 23.3 MB (38.47%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -7,60 +7,57 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
-        
-        # queue and get everything and popleft(), pop() nad connect the nodes that way 
-        
         """
-        # reverse the second half of the linked list 
-        # and set a pointer to the second half and iterate through it 
-        
 
-        dummy = ListNode()  
-        res = dummy
-        secondHalfPointer = head
+        # ok somehow you need to get nodes from beginning, end 
+        # beginning + 1, end - 1
+
+        # so i think maybe a way we could approach this problem is to reverse the second half
+        # we can have a dummy pointer and just merge one by one
+
+        # to find the middle, we use the trick 
+
+        # if it is odd, we want to increment + 1 due to edge case
+        # if it is even, it should not matter
+
+        # now we have to reverse this half 
+
+        dummy = ListNode()
+        answer = dummy
+
+        middle_node = self.obtain_middle(head)
+        reverse_list_pointer = self.reverse(middle_node)
+        first_half_pointer = head
+
+        while first_half_pointer and reverse_list_pointer:
+            dummy.next = first_half_pointer
+            first_half_pointer = first_half_pointer.next
+            dummy = dummy.next
+            dummy.next = reverse_list_pointer
+            reverse_list_pointer = reverse_list_pointer.next
+            dummy = dummy.next
         
-        slow, fast = head, head 
+        if first_half_pointer:
+            dummy.next = first_half_pointer
         
+        return answer.next
+
+    def obtain_middle(self, head):
+        middle, fast = head, head
+
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next 
-        
-        if fast:
-            # to account for the odd number lists, since we are iteraitng first half
-            # of the list first, we want to not include this in the thing 
-            slow = slow.next  
-        
-        while secondHalfPointer.next != slow:
-            secondHalfPointer = secondHalfPointer.next
-        
-        startofSecondHalf = self.reverse(slow)
-        
-        secondHalfPointer.next = startofSecondHalf
-        secondHalfPointer = secondHalfPointer.next
-        
-        curr = head 
-        while curr != startofSecondHalf:
-            dummy.next = curr
-            curr = curr.next 
-            dummy = dummy.next 
-            if secondHalfPointer:
-                # accounts for odd where there is more for the first half 
-                dummy.next = secondHalfPointer
-                secondHalfPointer = secondHalfPointer.next
-                dummy = dummy.next
-        dummy.next = None
-        return res.next
-        
-    def reverse(self, slow):
+            middle = middle.next
+            fast = fast.next.next
+        slow = middle
+        middle = middle.next
+        slow.next = None
+        return middle
+
+    def reverse(self, middle_node):
         prev = None
-        while slow:
-            temp = slow.next
-            slow.next = prev
-            prev = slow
-            slow = temp
+        while middle_node:
+            temp = middle_node.next
+            middle_node.next = prev
+            prev = middle_node
+            middle_node = temp
         return prev
-        
-        
-        
-        
-        
