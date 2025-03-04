282904eacd221d14928c7baa95593a7983220616
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-04 00:50:32 -0600

    Time: 104 ms (7.91%) | Memory: 18 MB (8.19%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        current_length = len(s)
+        input_string = s
+
+        while current_length > 2:
+            input_string = self.computeNew(input_string)
+            current_length = len(input_string)
+        return True if input_string[0] == input_string[1] else False
+    
+    def computeNew(self, input_string):
+        pointer = 0
+        res = []
+
+        while pointer < len(input_string) - 1:
+            first_char = input_string[pointer]
+            second_char = input_string[pointer + 1]
+            res.append((int(first_char) + int(second_char)) % 10)
+            pointer += 1
+        
+        return "".join(map(str, res))
