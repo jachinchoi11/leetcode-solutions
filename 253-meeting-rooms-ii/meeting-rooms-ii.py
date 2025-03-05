38375e2e2ec334e27e83c6f4370f737a6f8002ac
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-04 18:04:52 -0600

    Time: 7 ms (32.63%) | Memory: 19.9 MB (49.91%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,27 +1,41 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # sorted nlogn
 
-        startTime = [] 
-        endTime = []
-        currRooms, maxRooms = 0, 0
-        startP, endP = 0,0
+        # we want to minimze the amount of conference rooms, thinking of this just lgoically
+        # you want to kind of cram everyone in as much as possible, as in if you use a room,
+        # you want it to always be fileld
+        # in an ideal world, the intervals are not overlapping, as then you would onlu need one
+        # so try to fit it into one room, but add rooms, as you have to go if the prior ones you've already booked
+        # are being used
+
+        # so i'm thinking maybe we can potentially create a queue of some sort, where we could pop off 
+        # after checking if the end time is bigger than the current time 
+        # but that might not be it, cuause it is not sorting necessarily
+
+        # maybe we can use a heap that priorizies the end times? 
+            # so we would go through and have a start time, and before we add, pop off anything in there 
+            # if start time > time on the heap
+            # then we would go on and do that 
+            # i believe this sol would work, but it would be O (nlogn) solution
+        
+        pq = []
+        max_rooms = 0
+        intervals.sort() # sort it by start times
 
         for start, end in intervals:
-            startTime.append(start)
-            endTime.append(end)
+            while pq and pq[0] <= start:
+                heapq.heappop(pq)
+            
+            heapq.heappush(pq, end)
+
+            max_rooms = max(max_rooms, len(pq))
         
-        startTime.sort()
-        endTime.sort()
+        return max_rooms
+        
 
-        while startP < len(startTime) and endP < len(endTime):
 
-            if startTime[startP] < endTime[endP]:
-                currRooms += 1
-                startP += 1
-            else:
-                currRooms -= 1
-                endP += 1
-            maxRooms = max(maxRooms, currRooms)
 
-        return maxRooms
+
+
+        
+
