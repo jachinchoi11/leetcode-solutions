c51b3fe505566cb5d5d6db73c8f6a43ee923f900
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-08 13:14:30 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,40 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        # so here i think maybe we can use a stack in this case
+
+        # using a stack will allow us to keep track of which things we do have, and i was thinking 
+        # we just have store the open parentheses and then we can i guess pop it off if the same closing parentehse os there
+
+        # any time we deviate would be 
+            # when a closing bracket doesn't match --> immediately false
+            # when there is a closing bracket, but no openign --> immediatly false -->check
+            # also at the end if we have any opening ones that were'nt matched up --> false
+                # else True
+        # in terms of clarifyign questions, can we expect taht tehre will only be parenthese? 
+            # what about will there always be an openign brakcet first? 
+            # what do we do --> if there are other things --> skip
+
         
-        dictionary = {')' : '(' , '}': '{', ']' : '['}
+
         stack = []
-        
-        for char in s:
-            if char in dictionary:
-                if stack and stack[-1] == dictionary[char]:
+
+        match_map = {')' : '(', '}' : '{', ']': '['}
+
+        for paren in s:
+
+            if paren in ')]}':
+                if stack and stack[-1] == match_map[paren]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(char)
+                stack.append(paren)
         
+        return True if not stack else False
 
-        if stack:
-            return False
-        return True
+                
+
+
+
+
