5e814b3d1d347459b4ac23799691ec5036440c05
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-08 13:24:45 -0600

    Time: 35 ms (26.84%) | Memory: 27.1 MB (5.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,20 +1,36 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [1] * len(nums)
-        postfix = [1] * len(nums)
-        res = []
+
+        # initial answer for me would be to just go and get the total mutliplicative product
+        # and then go to each one and dividde the current number
+            # you can not use the division poperator
+
+        # so an observation of that is literally just the definiton 
+            # you have a left and a right operator 
+            # and you want to multiply everythign on your lieft * everything on your right
+        
+        # so maybe we make a left_product array and a right_product array
+        # precompute those values, and then just multiply it at the very end 
+            # three pass solutin
+                # O(n) time and O(n) space
+        
+        left_product = [1] * len(nums)
+        right_product = [1] * len(nums)
+        ans = [0] * len(nums)
+
         for index in range(1, len(nums)):
-            currNum = nums[index - 1] * prefix[index - 1]
-            prefix[index] = currNum
+            left_product[index] = left_product[index - 1] * nums[index - 1]
         
-        for index in range(len(nums) -2, -1,-1):
-            currNum = nums[index + 1] * postfix[index + 1]
-            postfix[index] = currNum
+        for index in range(len(nums) - 2, -1, -1):
+            right_product[index] = right_product[index + 1] * nums[index + 1]
         
-        for index in range(len(nums)):
-            res.append(postfix[index] * prefix[index])
+        for index in range(len(ans)):
+            ans[index] = left_product[index] * right_product[index]
         
-        return res
+        return ans
+        
+
 
         
+
         
