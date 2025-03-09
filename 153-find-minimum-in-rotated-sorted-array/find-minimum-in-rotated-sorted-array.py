9a57d7dd2f3c8f73f270a1a951db6fd55600371e
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-09 18:21:07 -0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (9.71%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,15 +1,25 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
 
+        # a binary search of some kind to find the minimum value 
+        # we would use two pointers in this case 
+
         left, right = 0, len(nums) - 1
 
         while left <= right:
-            middle = (left + right) // 2
-            if nums[right] > nums[middle]:
-                right = middle 
+            middle = left + (right - left) // 2
+            if nums[middle] < nums[right]:
+                # here we know we want to go down, however, that value in the middle might be the value we need
+                right = middle
             else:
+                # because we can rule it out bc the middle is bigger than right - meaning it can't be min
                 left = middle + 1
-        return nums[middle]
-    
+        return nums[middle] # because we have to return a converged value, we have to use <= 
 
+
+        # 3,4,5,1,2
+
+        # left = 0, right = 4
+
+        # left = 3 right = 3
 
