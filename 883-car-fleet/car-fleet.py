3d0e8b0dc2f7d0349fe4fbd4f5d56559b32fd55f
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-09 00:36:00 -0600

    Time: 172 ms (51.53%) | Memory: 37.3 MB (89.00%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,31 +1,33 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-
-        pairs = []
-
-        for index in range(len(position)):
-            currPosition = position[index]
-            currSpeed = speed[index]
-            pairs.append((currPosition, currSpeed))
         
+        # there are n cars reaching target
+            # they have diff sppedds and diff positions
+        
+        # limitation --> the car ahead of you if your speed is faster than thiers
+            # switch speeds to the car slower
+         
+        # if the limitation is that, we should calculate, when they would get to said target
+            # if you get there faster or same speed as someone ahead of you 
+                # then you become a fleet together 
+        
+        # maybe a good solution would be to start with the people that get thier first 
+            # we would have a stack of tuples 
+        
+        pairs = [(curr_position, curr_speed) for curr_position, curr_speed in zip(position, speed)]
         pairs.sort(reverse = True)
 
         stack = []
 
-        def computeTime(currCar, target):
-            currTarget = target
-            currTarget -= currCar[0]
-            ans = currTarget / currCar[1]
-            return ans
-        
-        for pair in pairs:
+        for curr_p, curr_s in pairs:
+            curr_time = self.calculate_time(curr_p, curr_s, target)
             if not stack:
-                stack.append(pair)
+                stack.append(curr_time)
             else:
-                if computeTime(stack[-1], target) >= computeTime(pair, target):
-                    continue
-                else:
-                    stack.append(pair)
+                if stack[-1] < curr_time:
+                    stack.append(curr_time)
         
         return len(stack)
-        
+
+    def calculate_time(self, curr_p, curr_s, target):
+        return (target - curr_p) / curr_s
