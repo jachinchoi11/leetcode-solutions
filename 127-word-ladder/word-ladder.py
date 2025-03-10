a3b6bd7fe8d68aa54e091633f4b4afc66b6f37a0
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 15:31:37 -0500

    Time: 8815 ms (5.00%) | Memory: 19 MB (57.12%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,59 +1,45 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # get closest to the place, or actually maybe we can start from the beginWord, as its hard t knpw
 
-        # and then do a shortest path bfs from the beginWord to the endWord 
+        # so we're given two words and a beginWord endWord
+            # we can only use the words in teh word list to change it 
 
-        connections = defaultdict(list)
+            # i think one thing we can do is to start with the beginning word
+                # make an adj list
+                # go through all the words and see if they only have a one letter difference to switch
+                # make a helper that will go through that and edit the adj list 
+            
+                # and then just add that into the bfs and keep on doing that until i hit final word
 
-        
-        queue = deque()
-        path = set()
-        queue.append(beginWord)
-        path.add(beginWord)
+        queue = deque([beginWord])
+        adj_list = defaultdict(list)
         res = 1
+        visited = set()
 
         while queue:
             for _ in range(len(queue)):
-                currWord = queue.popleft()
-
-                if currWord == endWord:
+                curr_word = queue.popleft()
+                visited.add(curr_word)
+                if curr_word == endWord:
                     return res
-                path.add(currWord)
-
-                if currWord not in connections:
-                    self.findConnections(currWord, connections, wordList)
-
-                for connectedWord in connections[currWord]:
-                    if connectedWord not in path:
-                        queue.append(connectedWord)
-            
-            res += 1
-        
-        return 0
-    
-
-    def findConnections(self, findWord, connections, wordList):
-        for word in wordList:
-            diff = 0
-            for index in range(len(word)):
-                if word[index] != findWord[index]:
-                    diff += 1
-                    if diff >= 2:
-                        break
-            if diff <= 1 and diff > 0:
-                connections[findWord].append(word)
-        
-
-
-
-
-
-        
-
-
                 
-                    
+                if curr_word not in adj_list:
+                    self.find_connections(curr_word, wordList, adj_list)
+                
+                for neighbor in adj_list[curr_word]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+            res += 1
+        return 0
 
+    def find_connections(self, curr_word, wordList, adj_list):
 
-
+        for word in wordList:
+            changes = 0
+            for index in range(len(word)):
+                if curr_word[index] != word[index]:
+                    changes += 1
+                if changes == 2:
+                    break
+            if changes < 2:
+                adj_list[curr_word].append(word)
