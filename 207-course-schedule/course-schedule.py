7af685738a4ae919097ccde4fc5ff3d94706da30
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 14:31:18 -0500

    Time: 2 ms (91.67%) | Memory: 20.2 MB (6.91%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -6,30 +6,32 @@ class Solution:
         
         # essentially, we can start at course 0, and if we ever run into anything again gg 
 
-        path = set()
-        visited = set()
+        path = set() # set to track where you are right now
+        visited = set() # set to track cleared courses 
         adj_list = defaultdict(list) # will store from pre req to class 
 
         for start, end in prerequisites:
-            adj_list[start].append(end)
+            adj_list[end].append(start)
     
         def dfs(curr_course):
             if curr_course in path:
+                # this represents the path that we are taking 
                 return False
             if curr_course in visited:
                 return True
-            
             path.add(curr_course)
 
             for next_course in adj_list[curr_course]:
                 if not dfs(next_course):
                     return False
-            path.remove(curr_course)
-            visited.add(curr_course)
+            path.remove(curr_course) # so must be removed, as we have already dealit with it
+            visited.add(curr_course) # using the visited, so that if we go to a course that is checked already, then it should be fine
             return True
         
+
         for course in range(numCourses):
             if not dfs(course):
                 return False
+        
         return True
         
