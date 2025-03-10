17ea2c74b19769d533c367aed45ee79d7767d1e8
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-09 19:00:58 -0500

    Time: 151 ms (79.00%) | Memory: 18.9 MB (49.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,29 +1,39 @@
+import math
 class Solution:
-    import math
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        # we want to find the minimum rate of time it wil take to finish in h hours
+        # so initally, im thinkign waht if we just start 1 to the max --> and try and see if that will work
+            # the first time it will work 
         
-        minRate = 1 
-        maxRate = max(piles)
 
+        # so i'm thinking we can potentially speed up this process as well, max time --> the max amount in a pile
+            # im thiking we can do a binary search of some sort that biases minimum values
+            # we can have a helper funciton whether it is possible in h hours, and always try to shrink if possible 
 
-        while minRate < maxRate:
-            
-            middleRate = (minRate + maxRate) // 2
-            hours = self.calculateHours(middleRate, piles)
+        max_value = max(piles) # O(n) operation, where we look for max in piles
+        min_value = 1
+        ans = float('inf')
 
-            if hours <= h:
-                maxRate = middleRate
+        while min_value <= max_value:
+            middle_rate = min_value + (max_value - min_value) // 2
+            eligible = self.is_valid(piles, middle_rate, h)
+            if eligible:
+                ans = min(ans, middle_rate)
+                max_value = middle_rate - 1
             else:
-                minRate =  middleRate + 1
-        return minRate
-
+                min_value = middle_rate + 1
+        return ans 
+    
+    def is_valid(self, piles, rate, h):
+        time = 0
+        for pile in piles:
+            time += math.ceil(pile/rate)
+        return time <= h
+        
+        
         
 
-    def calculateHours(self, middleRate, piles):
-        res = 0
-        # keeps track of how many hours 
-        for num in piles:
-            res += ceil(num/middleRate)
-        return res 
 
-                
+
+        
