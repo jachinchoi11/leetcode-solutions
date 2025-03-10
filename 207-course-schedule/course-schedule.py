aa71ea3b25a572093a347f65dfb84f8c799733f5
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 15:01:22 -0500

    Time: 3 ms (89.09%) | Memory: 19 MB (83.25%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,37 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        # if you can finish all the courses --> essentially, as long as we don't have a cycle
-            # as in theres no we havet o have compelteed this to take that 
-        
-        # essentially, we can start at course 0, and if we ever run into anything again gg 
 
-        path = set() # set to track where you are right now
-        visited = set() # set to track cleared courses 
-        adj_list = defaultdict(list) # will store from pre req to class 
 
-        for start, end in prerequisites:
-            adj_list[end].append(start)
-    
-        def dfs(curr_course):
-            if curr_course in path:
-                # this represents the path that we are taking 
-                return False
-            if curr_course in visited:
-                return True
-            path.add(curr_course)
+        indegree = [0] * numCourses
+        adj_list = defaultdict(list)
+        courses_done = 0
 
-            for next_course in adj_list[curr_course]:
-                if not dfs(next_course):
-                    return False
-            path.remove(curr_course) # so must be removed, as we have already dealit with it
-            visited.add(curr_course) # using the visited, so that if we go to a course that is checked already, then it should be fine
-            return True
+        for course, prereq in prerequisites:
+            adj_list[prereq].append(course) # prereq --> course
+            indegree[course] += 1
         
+        queue = deque([i for i in range(len(indegree)) if indegree[i] == 0])
 
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
+        while queue:
+            curr_course = queue.popleft()
+            courses_done += 1
+            for other_course in adj_list[curr_course]:
+                # here, if we have processed it already, that means that there is less than one prereq
+                indegree[other_course] -= 1
+                if indegree[other_course] == 0:
+                    # whenever it has no prereqs left that is when we should take it out
+                    queue.append(other_course)
         
-        return True
-        
+        return courses_done == numCourses
+
