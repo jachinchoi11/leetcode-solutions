c83eb3180b533702befcfc8b04d6e07b50a84ff4
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 21:18:40 -0500

    Time: 193 ms (5.00%) | Memory: 26.9 MB (77.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -7,17 +7,13 @@ class Solution:
         # if we see a minimum number, we want to shift l += 1, where that is not the case
 
         
-        left, right = 0, 0
+        min_price = float('inf')
         max_profit = 0
 
-        while right < len(prices):
-            
-            curr_profit = prices[right] - prices[left]
-            while prices[right] < prices[left]:
-                left += 1
-                curr_profit = prices[right] - prices[left]
-            max_profit = max(curr_profit, max_profit)
-            right += 1
+        for index in range(len(prices)):
+            min_price = min(min_price, prices[index])
+            max_profit = max(max_profit, prices[index] - min_price)
+        
         
         return max_profit
             
