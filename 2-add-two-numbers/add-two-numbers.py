f01c69b98e1c5235a2ee3b33aba3e377a05fa9f0
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 22:01:54 -0500

    Time: 11 ms (10.29%) | Memory: 18.1 MB (14.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,55 +5,65 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # the part about this is that there is some form of carry 
+        # so if the sum of the values are over 10 then we have to carry add (number // 10) 
+        # and you add number % 10
+    
+
         dummy = ListNode()
         prev = dummy
         carry = 0
 
         while l1 and l2:
-            currNum = 0
-
-            currNum = l1.val + l2.val + carry
+            curr_sum = l1.val + l2.val
+            print(curr_sum)
             if carry > 0:
-                carry -= 1
-            if currNum >= 10:
-                currNum -= 10
-                carry += 1
+                curr_sum += carry
+                carry = 0
+            
+            carry += (curr_sum // 10)
 
-            dummy.next = ListNode(currNum)
+            new_node = ListNode(curr_sum % 10)
+            dummy.next = new_node
+            dummy = dummy.next
             l1 = l1.next
             l2 = l2.next
-            dummy = dummy.next
         
         while l1:
-            currSum = l1.val + carry
-            if currSum >= 10:
-                currSum -= 10
-                carry += 1
+            curr_sum = l1.val
+
             if carry > 0:
-                carry -= 1
-            dummy.next = ListNode(currSum)
+                curr_sum += (carry)
+                carry = 0
+            
+            carry += (curr_sum // 10)
+
+            new_node = ListNode(curr_sum % 10)
+            dummy.next = new_node
             dummy = dummy.next
-            l1 = l1.next      
+            l1 = l1.next
         
-        while l2: 
-            currSum = carry + l2.val
-            if currSum >= 10:
-                currSum -= 10
-                carry += 1
+        while l2:
+            curr_sum = l2.val
+
             if carry > 0:
-                carry -= 1
-            dummy.next = ListNode(currSum)
+                curr_sum += (carry)
+                carry = 0
+            
+            carry += (curr_sum // 10)
+
+            new_node = ListNode(curr_sum % 10)
+            dummy.next = new_node
             dummy = dummy.next
-            l2 = l2.next 
-        if carry != 0:
+            l2 = l2.next
+        
+        if carry > 0:
             dummy.next = ListNode(carry)
         
         return prev.next
+
+
         
+
+
         
-
-
-
-
-
-            
