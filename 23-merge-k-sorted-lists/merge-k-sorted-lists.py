5f80d5b5ffee0875463d287d3a8e24739526d74d
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-11 15:44:00 -0500

    Time: 11 ms (64.37%) | Memory: 20.4 MB (19.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,42 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        # merge all the sorted linked lists and put them all together 
-
-        # so i thin ka great way to do this is knowing that they all are sorted, we could just move all the values into a heap 
-        # and then just pop them off and form it that way
-
-        # or we can maybe do a copmarison for each of the nodes, so we'll have a pointer from each of the lists, and then we'll get one thing from each place
-        # so essetnailly, pointers for all of them,to keep track of them, we can have thier value - first, row and column, and then the ListNode()
-        
-        # if they are the same 
-            # then we can just take whateer, doesnt matter
-        rows = len(lists)
+    
         heap = []
-        dummy_node = ListNode()
-        prev = dummy_node
 
-        for row in range(rows):
-            # we're building our initial heap 
-            if lists[row]:
-                heapq.heappush(heap, (lists[row].val, row, lists[row]))
-                lists[row] = lists[row].next
-            
+        dummy = ListNode(0)
+        answer = dummy
+
+        for index, curr_list in enumerate(lists):
+            if curr_list:
+                heapq.heappush(heap, (curr_list.val, index, curr_list))
+                curr_list = curr_list.next
         
         while heap:
-            # this would be self sufifcient, because we woudl always try to add more if there is 
-            # we can not index through this 
-            _, curr_row, curr_node = heapq.heappop(heap)
-            dummy_node.next = curr_node
-            dummy_node = dummy_node.next
 
-            if lists[curr_row]:
-                heapq.heappush(heap, (lists[curr_row].val, curr_row, lists[curr_row]))
-                lists[curr_row] = lists[curr_row].next
+            _, curr_index, min_node = heapq.heappop(heap)
 
-        return prev.next
-            
-            
+            dummy.next = min_node
+            dummy = dummy.next
+        
+            if min_node.next:
+                heapq.heappush(heap, (min_node.next.val, curr_index, min_node.next))
+                min_node = min_node.next
+        return answer.next
 
         
