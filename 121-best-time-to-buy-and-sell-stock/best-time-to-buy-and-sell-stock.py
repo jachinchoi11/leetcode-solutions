a511248c380986bf98219c7ca6e3ac9cc6304a0d
Jachin Choi <156959357+jachinchoi11@users.noreply.github.com>
2025-03-10 21:14:25 -0500

    Time: 139 ms (16.59%) | Memory: 26.9 MB (77.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        l, r = 0,0
-        maxProfit = 0
-        while r < len(prices):
-            profit = prices[r] - prices[l]
-            while prices[r] < prices[l]:
-                l = r
-            maxProfit = max(maxProfit, profit)
-            r += 1
+        # so essentially we want to get the maximum profit from this list 
+            # we can buy one time and sell it another time 
         
-        return maxProfit 
+        # if we see a minimum number, we want to shift l += 1, where that is not the case
+
+        
+        left, right = 0, 0
+        max_profit = 0
+
+        while right < len(prices):
+            
+            curr_profit = prices[right] - prices[left]
+            while prices[right] < prices[left]:
+                left += 1
+                curr_profit = prices[right] - prices[left]
+            max_profit = max(curr_profit, max_profit)
+            right += 1
+        
+        return max_profit
+            
